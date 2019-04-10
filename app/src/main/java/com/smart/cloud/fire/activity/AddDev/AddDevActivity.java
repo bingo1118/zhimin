0f786622cb6c918fab2848d7e95fb1f6a763f2c0
bingo1118 <447292486@qq.com>
2019-04-10 09:58:25 +0800

    2019.04.10数据备份

---
+++ app/src/main/java/com/smart/cloud/fire/activity/AddDev/AddDevActivity.java
@@ -0,0 +1,16 @@
+package com.smart.cloud.fire.activity.AddDev;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import fire.cloud.smart.com.smartcloudfire.R;
+
+public class AddDevActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dev);
+    }
+}
